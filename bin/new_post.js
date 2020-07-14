582fdb3572c1e96a8eb90a994215731426e3619e
Scott Watermasysk <scottwater@gmail.com>
2020-07-14 15:38:15 -0400

    silent spacebar

--- bin/new_post.js
+++ bin/new_post.js
@@ -2,28 +2,20 @@ const fs = require("fs");
 const { v4: uuidv4 } = require("uuid");
 
 const argv = process.argv;
-const type = argv[2];
-const fileName = argv[3];
+const fileName = argv[2];
 
-if (type && fileName && ["post", "short", "link"].includes(type)) {
-  const frontMatter = `
+const frontMatter = `
 ---
 title:
 date: ${new Date().toISOString()}
 uuid: ${uuidv4()}
-${type === "link" ? "source_url:" : ""}
 `.trim();
-  const filePath = `site/${type}/${fileName}.md`;
-  fs.writeFile(filePath, `${frontMatter}\n---\n`, (err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      var spawn = require("child_process").spawn;
-      spawn("code", [filePath]);
-    }
-  });
-} else {
-  console.log(
-    "You must specify a type and file name. Type must be post, short, or link"
-  );
-}
+const filePath = `site/posts/${fileName}.md`;
+fs.writeFile(filePath, `${frontMatter}\n---\n`, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    var spawn = require("child_process").spawn;
+    spawn("code", [filePath]);
+  }
+});
---
+++ site/posts/silent-spacebar.md
@@ -0,0 +1,18 @@
+---
+title: Silent Spacebar
+date: 2020-07-14T19:20:28.958Z
+uuid: 5b9c2227-3f25-4ce4-a2a8-c2cbfa589729
+previewImage: /assets/images/sound.png
+---
+
+On the [KBD67 MKII build post](https://splitbackspace.com/posts/kbd67-mkii/), I mentioned switching to a silent switch on the spacebar.
+
+This is not something I expect to do all the time, but it was a big improvement with the Zykos on a brass plate. I felt it would be good to highlight the two sound profiles below if you wanted to do a quick side by side comparison.
+
+## Zykos on all the keys
+
+https://youtu.be/9mMosHKlnh8
+
+## Healio on the spacebar, Zykos on the rest of the keys
+
+https://youtu.be/MMyvfpUWymw
