f0e1735abbbf4df8be899cf6cb5ec3f4e6c69276
Scott Watermasysk <scottwater@gmail.com>
2020-08-03 13:18:46 -0400

    whitelist playing CSS

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,12 +1,13 @@
 module.exports = {
   content: ["./dist/**/*.html"],
   css: ["./dist/assets/*.css"],
+  whitelist: ["playing", "stop-button", "play-button"],
   extractors: [
     {
-      extractor: function(content) {
-          return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
+      extractor: function (content) {
+        return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
       },
-      extensions: ["html"]
-    }
-  ]
+      extensions: ["html"],
+    },
+  ],
 };
